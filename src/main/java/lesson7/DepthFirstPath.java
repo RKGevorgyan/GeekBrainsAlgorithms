605f23a1091376618895e1db597bbe0063b696d6
robert_gevorgyan <gevorgyanrk@gmail.com>
2021-04-06 23:27:31 +0300

    Домашнее задание к уроку 7

---
+++ src/main/java/lesson7/DepthFirstPath.java
@@ -0,0 +1,21 @@
+package lesson7;
+
+import java.util.LinkedList;
+
+public class DepthFirstPath extends Search{
+
+    public DepthFirstPath(Graph g, int source) {
+       super(g,source);
+        dfs(g, source);
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        for (int w : g.getAdjList(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(g, w);
+            }
+        }
+    }
+}
